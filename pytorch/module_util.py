6f1101b9edc9fd960d5dc871926ca650d4e30f7b
Yoshitomo Matsubara <akitomo4reg@yahoo.co.jp>
2020-02-15 13:15:39 -0800

    fixed a bug for nn.Sequential

--- pytorch/module_util.py
+++ pytorch/module_util.py
@@ -31,8 +31,14 @@ def get_module(root_module, module_path):
     module = root_module
     for module_name in module_names:
         if not hasattr(module, module_name):
-            return None
-        module = getattr(module, module_name)
+            if isinstance(module, nn.Sequential):
+                module = module[int(module_name)]
+            else:
+                print('`{}` of `{}` could not be reached in `{}`'.format(module_name, module_path,
+                                                                         type(root_module).__name__))
+                return None
+        else:
+            module = getattr(module, module_name)
     return module
 
 
